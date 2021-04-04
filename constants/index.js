01ea9151bba95431921463dfaa9fe956e293d1a5
abraham ghaemi <ig.ghaemi@gmail.com>
2021-04-04 06:58:17 +0430

    update constant files

---
+++ constants/index.js
@@ -0,0 +1,8 @@
+import icons from "./icons"
+import images from "./images"
+import { COLORS, FONTS, SIZES } from "./theme"
+import GOOGLE_MAP_API_KEY from "./maps"
+
+export {
+    icons, images, COLORS, FONTS, SIZES, GOOGLE_MAP_API_KEY
+}
